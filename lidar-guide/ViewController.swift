a9dee1459359d7446ea7cfffa69bd7573fd0916e
bhborba <brunohenriqueborba@gmail.com>
2021-09-02 14:12:31 -0300

    Initial Commit

---
+++ lidar-guide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lidar-guide
+//
+//  Created by Bruno de Borba on 02/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
